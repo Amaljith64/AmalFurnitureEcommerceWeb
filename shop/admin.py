482536730fbfea3a27cb53306f72a9cc09796136
Amaljith <amaljith64@gmail.com>
2022-02-10 18:55:19 +0530

    model completed

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+
+class catadmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}  # prepopulated is used to add slug automatically while typping name
+admin.site.register(categ,catadmin)
+
+class prodadmin(admin.ModelAdmin):
+   prepopulated_fields = {'slug':('name',)}
+admin.site.register(products,prodadmin)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class categ(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+
+class products(models.Model):
+    name=models.CharField(max_length=250,unique=True )
+    slug=models.SlugField(max_length=250,unique=True)
+    desc=models.TextField()
+    img=models.ImageField()
+    price=models.IntegerField()
+    stock=models.IntegerField()
+    available=models.BooleanField()
+    category=models.ForeignKey(categ,on_delete=models.CASCADE)
