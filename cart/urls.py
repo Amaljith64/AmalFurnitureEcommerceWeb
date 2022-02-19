31504c563df11f7abe66ee4a66b238e72da5f4d5
Amaljith <amaljith64@gmail.com>
2022-02-19 12:54:53 +0530

    Items can be added to cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns=[
-    path('cartDetails',views.cart_details,name='cartDetails')
+    path('cartDetails',views.cart_details,name='cartDetails'),
+    path('add/<int:product_id>/',views.add_cart,name='addcart')
 
 
 ]
